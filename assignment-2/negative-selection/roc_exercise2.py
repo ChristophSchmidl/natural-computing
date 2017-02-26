6c9f9f0d33494a904d5a5fac0c273213033b4fad
Lumos <runner.lex@gmail.com>
2017-02-26 23:18:08 +0100

    Added the remaining ROCs and the pdf was added.

--- assignment-2/negative-selection/roc_exercise2.py
+++ assignment-2/negative-selection/roc_exercise2.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import roc_curve, auc
 from pprint import pprint
 
-file_name_true = 'syscalls/snd-cert/snd-cert.2.labels'
-file_name_pred = 'syscalls/snd-cert/10/scores/snd-cert.3_r_{}.score'
+file_name_true = 'syscalls/snd-unm/snd-unm.1.labels'
+file_name_pred = 'syscalls/snd-unm/10/scores/snd-unm.1_r_{}.score'
 
 fpr = dict()
 tpr = dict()
@@ -12,8 +12,9 @@ roc_auc = dict()
 
 for i in range(1,10):
     with open(file_name_true.format(i), 'r') as true_file, open(file_name_pred.format(i), 'r') as pred_file:
-		true = map(float, true_file.read().splitlines()[:99])
-		pred = map(float, pred_file.read().splitlines()[:99])
+		true = np.array(map(float, true_file.read().splitlines()[:99]))
+		pred = np.array(map(float, pred_file.read().splitlines()[:99]))
+		pred = pred/float(max(pred))
 		fpr[i], tpr[i], _ = roc_curve(true, pred)
 		roc_auc[i] = auc(fpr[i], tpr[i])
 
@@ -25,6 +26,6 @@ plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.title('CHECK, labels cert 2, predictions cert 3')
+plt.title('Receiver operating characteristic curve exercise 2 unm 1')
 plt.legend(loc="lower right")
 plt.show()
